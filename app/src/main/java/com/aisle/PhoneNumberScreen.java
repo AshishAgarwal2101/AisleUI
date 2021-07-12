3ea5ab635dbc786c55fc1a97d60bde6dbfd2c9a9
Ashish <ashish21.01agarwal@gmail.com>
2021-07-12 20:51:20 +0530

    Code refactor

--- app/src/main/java/com/aisle/PhoneNumberScreen.java
+++ app/src/main/java/com/aisle/PhoneNumberScreen.java
@@ -3,11 +3,11 @@ package com.aisle;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class OtpScreen extends Activity {
+public class PhoneNumberScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp_screen);
+        setContentView(R.layout.activity_phone_number);
     }
 }
