49fe36a4facccad5e5ff5bb9749f8feb30a2db00
Ashish <ashish21.01agarwal@gmail.com>
2021-07-12 21:50:13 +0530

    Connectivity of screens

--- app/src/main/java/com/aisle/MainActivity.java
+++ app/src/main/java/com/aisle/MainActivity.java
@@ -1,14 +1,42 @@
 package com.aisle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView otpScreenButton = findViewById(R.id.main_screen_otp_button);
+        TextView phoneNumberScreenButton = findViewById(R.id.main_screen_phone_number_button);
+        TextView homeScreenButton = findViewById(R.id.main_screen_home_button);
+
+        otpScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),OtpScreen.class);
+                startActivity(i);
+            }
+        });
+        phoneNumberScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),PhoneNumberScreen.class);
+                startActivity(i);
+            }
+        });
+        homeScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
