dee36078868b6192133bb7edc97ec687b8f9303d
Ashish <ashish21.01agarwal@gmail.com>
2021-07-12 20:46:45 +0530

    Code refactor

--- app/src/main/java/com/aisle/OtpScreen.java
+++ app/src/main/java/com/aisle/OtpScreen.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class otpScreen extends AppCompatActivity {
+public class OtpScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
